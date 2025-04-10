3ad233da0198e476f9a90f8a583f04de180ab2c6
Srikanth Anusuri <sanusuri@starbucks.com>
2025-04-09 21:21:16 -0500

    Usage of parameters

--- graphql-client/app/components/movieList.tsx
+++ graphql-client/app/components/movieList.tsx
@@ -2,32 +2,38 @@ import { useQuery } from "@apollo/client/react/hooks";
 import { gql } from "graphql-tag";
 
 const movies = gql`
-query Movies {
-  movies {
-    _id
+query MovieById($id: ID!) {
+  movieById(_id: $id) {
+    title
+    plot
     fullplot
     genres
     countries
-    plot
-    title
   }
 }
 `;
 
+function renderCard(movie) {
+    return (<div key={movie._id} style={{margin: '1rem 0.25rem'}}>
+        <div>Id: {movie._id}</div>
+        <div>Title: {movie.title}</div>
+        <div>Plot: {movie.plot}</div>
+        <div>Full Plot: {movie.fullplot}</div>
+        <div>Genres: {movie.genres?.join(',')}</div>
+        <div>Countries: {movie.countries?.join(',')}</div>
+    </div>);
+}
 
 export default function MovieList () {
-    const {loading, data} = useQuery(movies);
+    const {loading, data} = useQuery(movies, {
+        variables: {
+            id: "573a1390f29313caabcd4135"
+        }
+    });
     return (
         <div>
-            {loading ? <>Loading movies...</> : data.movies.map((movie) => (
-                <div key={movie._id} style={{margin: '1rem 0.25rem'}}>
-                    <div>Title: {movie.title}</div>
-                    <div>Plot: {movie.plot}</div>
-                    <div>Full Plot: {movie.fullplot}</div>
-                    <div>Genres: {movie.genres?.join(',')}</div>
-                    <div>Countries: {movie.countries?.join(',')}</div>
-                </div>
-                ))}
+            {loading ? <>Loading movies...</> : data.movies?.map(renderCard)}
+            { data.movieById && <>{renderCard(data.movieById)}</> }
         </div>
     );
 }
